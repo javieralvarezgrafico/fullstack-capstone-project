337d5f3af1271b6d29f49999e9061a0d82d1d7c8
javieralvarezgrafico <javier.alvarez.grafico@gmail.com>
2026-02-14 15:06:32 -0500

    Commit after deleting some comments and polishing giftRoutes.js due to some error handling differences between my version and the suggested solution

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,11 +23,9 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
 const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
 const searchRoutes = require('./routes/searchRoutes');
 
 
@@ -38,11 +36,9 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
 app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
 app.use('/api/gifts', searchRoutes);
 
 
