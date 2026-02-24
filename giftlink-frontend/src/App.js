fd58319dd5bf99e78e522150fc9a4682d179bbdb
javieralvarezgrafico <34449340+javieralvarezgrafico@users.noreply.github.com>
2026-02-24 17:20:51 +0100

    Update App.js
    Modified line 2 and 26 of App.js from giftlink-frontend

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -23,7 +23,7 @@ function App() {
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/search" element={<SearchPage/>} />
-          
+          <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
   );
